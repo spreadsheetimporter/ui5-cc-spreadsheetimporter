2c68b1ed1c759ce5246cf5dfc7bdb48bf2916f39
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2022-12-29 15:59:12 +0100

    feat: new upload test

--- examples/test/specs/v4/BasicOpenDialog.test.js
+++ examples/test/specs/v4/BasicOpenDialog.test.js
@@ -15,6 +15,13 @@ describe("Open Excel Upload dialog", () => {
 					entitySet: "Orders"
 				}
 			},
+			onTheSubDetailPage: {
+				ObjectPage: {
+					appId: "ui.v4.orders",
+					componentId: "Orders_ItemsObjectPage",
+					entitySet: "Orders_Items"
+				}
+			},
 			onTheShell: {
 				Shell: {}
 			}
@@ -41,17 +48,6 @@ describe("Open Excel Upload dialog", () => {
 	});
 
 	it("Open ExcelUpload Dialog V4", async () => {
-		// open Object Page
-		// await browser.$('//*[@id="ui.v4.orders::OrdersList--fe::table::Orders::LineItem-innerTableRow-__clone6_cell1"]').click();
-		// // Open Edit Mode
-		// await browser
-		// 	.asControl({
-		// 		selector: {
-		// 			id: "ui.v4.orders::OrdersObjectPage--fe::StandardAction::Edit"
-		// 		}
-		// 	})
-		// 	.press();
-		// open Excel Dialog
 		await browser
 			.asControl({
 				selector: {
@@ -71,7 +67,102 @@ describe("Open Excel Upload dialog", () => {
 		expect(excelUploadDialog.isOpen()).toBeTruthy();
 	});
 
+	it("Upload file", async () => {
+		const uploader = await browser
+		.asControl({
+			forceSelect: true,
+			
+			selector: {
+				interaction: "root",
+				controlType: "sap.ui.unified.FileUploader",
+				id: "__uploader0"
+			}
+		});
+		const fileName = "test/TwoRowsNoErrors.xlsx" // relative to wdio.conf.(j|t)s
+		const remoteFilePath = await browser.uploadFile(fileName) // this also works in CI senarios!
+		// transition from wdi5 api -> wdio api
+		const $uploader = await uploader.getWebElement() // wdi5
+		const $fileInput = await $uploader.$("input[type=file]") // wdio
+		await $fileInput.setValue(remoteFilePath) // wdio
+		await browser.asControl({
+			selector: {
+				controlType: "sap.m.Button",
+				properties: {
+					text: "Upload"
+				}
+			}
+		}).press();
+	});
+
+	it("execute save", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			When.onTheDetailPage.onFooter().iExecuteSave()
+		});
+	});
+
+	it("go to Sub Detail Page", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheDetailPage.onTable({ property: "Items" }).iCheckRows({ ID: "254" });
+			When.onTheDetailPage.onTable({ property: "Items" }).iPressRow({ ID: "254" });
+		});
+	});
+
+	it("check Field: Quantity", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.iSeeThisPage();
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "quantity" }, { value: "3"});
+		});
+	});
+
+	it("check Field: Product", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "title" }, { value: 'Product Test 2'});
+		});
+	});
+
+	it("check Field: UnitPrice", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "price" }, { value: '13,7'});
+		});
+	});
+
+	it("check Field: validFrom", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "validFrom" }, { value: '25.11.2024, 01:00:00'});
+		});
+	});
+
+	it("check Field: timestamp", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "timestamp" }, { value: '24.11.2024, 01:00:00'});
+		});
+	});
+
+	it("check Field: date", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "date" }, { value: '23.11.2024'});
+		});
+	});
+
+	it("check Field: time", async () => {
+		await FioriElementsFacade.execute((Given, When, Then) => {
+			Then.onTheSubDetailPage.onForm("OrderItems").iCheckField({ property: "time" }, { value: '16:00:00'});
+		});
+	});
+
+	
+
+
+
 	after(async () => {
+		it("should see an object page", async () => {
+			await FioriElementsFacade.execute((Given, When, Then) => {
+				When.onTheShell.iNavigateBack()
+				Then.onTheMainPage.iSeeThisPage()
+				Given.onTheMainPage.onFilterBar().iExecuteSearch()
+				Then.onTheMainPage.onTable().iCheckRows({ identifier: "inc_0002", title: "Password obliteration" })
+			})
+		})
 		await browser
 			.asControl({
 				selector: {
