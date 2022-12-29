8834db6ba6487d906eafbc7a15284999bed4a4e3
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2022-12-29 16:17:25 +0100

    feat: updatr

--- examples/test/wdioV2.conf.js
+++ examples/test/wdioV2.conf.js
@@ -112,7 +112,7 @@ exports.config = {
 	// with `/`, the base url gets prepended, not including the path portion of your baseUrl.
 	// If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
 	// gets prepended directly.
-	baseUrl: "http://localhost:8081/index.html",
+	baseUrl: "http://localhost:8081/index.html?sap-language=EN",
 	//
 	// Default timeout for all waitFor* commands.
 	waitforTimeout: 10000,
