3faee59377384df1eb978ae82a7673ac1723b5c3
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-01-05 21:53:12 +0100

    chore: add typescript setup

--- dev/chokidar.js
+++ dev/chokidar.js
@@ -15,6 +15,7 @@ chokidar
 	})
 	.on("change", (event, path) => {
 		console.log(event, path);
+		shell.exec("babel src --out-dir webapp --source-maps true --extensions \".ts,.js\" --copy-files")
 		shell.exec("node ./dev/replace-string.js --develop");
 		shell.exec(
 			"ui5 build --config=ui5-build.yaml --all --clean-dest --dest dist  --exclude-task=replaceCopyright replaceVersion generateFlexChangesBundle generateVersionInfo minify escapeNonAsciiCharacters "
