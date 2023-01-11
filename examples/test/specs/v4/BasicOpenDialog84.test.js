3edb1e2a83f4546a337981c7a76480cd94c8e90e
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-01-11 23:32:12 +0100

    chore: update test

--- examples/test/specs/v4/BasicOpenDialog84.test.js
+++ examples/test/specs/v4/BasicOpenDialog84.test.js
@@ -15,49 +15,63 @@ describe("Open Excel Upload dialog", () => {
 	};
 
 	it("should trigger search on ListReport page", async () => {
-		await browser
+			await browser
 			.asControl({
 				selector: {
-					controlType: "sap.m.Button",
-					viewId: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ListReport.view.ListReport::Orders",
-					properties: {
-						text: "Go"
-					}
+					id: "ui.v4.ordersv4fe::OrdersList--fe::FilterBar::Orders-btnSearch"
 				}
 			})
 			.press();
 	});
 
 	it("go to object page", async () => {
-		await browser
-			.asControl({
-				selector: {
-					controlType: "sap.m.ColumnListItem",
-					viewId: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ListReport.view.ListReport::Orders",
-					bindingPath: {
-						path: "/Orders(ID=guid'64e718c9-ff99-47f1-8ca3-950c850777d4',IsActiveEntity=true)",
-						propertyPath: "IsActiveEntity"
-					}
+		const table = await browser.asControl({
+			selector: {
+				interaction: "root",
+				id: "ui.v4.ordersv4fe::OrdersList--fe::table::Orders::LineItem-innerTable"
+			}
+		});
+		const items = await table.getItems();
+		for (let index = 0; index < items.length; index++) {
+			const element = items[index];
+			const binding = await element.getBindingContext();
+			const object = await binding.getObject();
+			if (object.OrderNo === "2") {
+				const $element = await element.getWebElement();
+				try {
+					await $element.click();
+				} catch (error) {
+					// click failed, try again in a second
+					console.log(error);
+					try {
+						await $("filtekuzfutkfk424214").waitForExist({ timeout: 1000 });
+					} catch (error) {}
+					await $element.click();
 				}
-			})
-			.press();
+				break;
+			}
+		}
+		// force wait to stabelize tests
+		try {
+			await $("filtekuzfutkfk424214").waitForExist({ timeout: 1000 });
+		} catch (error) {}
 	});
 
 	it("go to edit mode", async () => {
 		await browser
 			.asControl({
 				selector: {
-					id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::Orders--edit"
+					id: "ui.v4.ordersv4fe::OrdersObjectPage--fe::StandardAction::Edit"
 				}
 			})
 			.press();
 	});
 
-	it("Open ExcelUpload Dialog V2", async () => {
+	it("Open ExcelUpload Dialog V4", async () => {
 		await browser
 			.asControl({
 				selector: {
-					id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::Orders--action::excelUploadButton"
+					id: "ui.v4.ordersv4fe::OrdersObjectPage--fe::CustomAction::excelUpload"
 				}
 			})
 			.press();
@@ -89,6 +103,10 @@ describe("Open Excel Upload dialog", () => {
 		const $uploader = await uploader.getWebElement(); // wdi5
 		const $fileInput = await $uploader.$("input[type=file]"); // wdio
 		await $fileInput.setValue(remoteFilePath); // wdio
+		// force wait to stabelize tests
+		try {
+			await $("filtekuzfutkfk424214").waitForExist({ timeout: 500 });
+		} catch (error) {}
 		await browser
 			.asControl({
 				selector: {
@@ -105,24 +123,22 @@ describe("Open Excel Upload dialog", () => {
 		await browser
 			.asControl({
 				selector: {
-					id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::Orders--activate"
+					id: "ui.v4.ordersv4fe::OrdersObjectPage--fe::FooterBar::StandardAction::Save"
 				}
 			})
 			.press();
 	});
 
 	it("go to Sub Detail Page", async () => {
-		try {
-			await $("filtekuzfutkfk424214").waitForExist({ timeout: 1000 });
-		} catch (error) {}
+		// try {
+		// 	await $("filtekuzfutkfk424214").waitForExist({ timeout: 1000 });
+		// } catch (error) {}
 		const table = await browser.asControl({
 			selector: {
-				controlType: "sap.m.Table",
-				viewId: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::Orders",
-				interaction: "root"
+				interaction: "root",
+				id: "ui.v4.ordersv4fe::OrdersObjectPage--fe::table::Items::LineItem-innerTable"
 			}
 		});
-		const metadata = await table.getMetadata();
 		const items = await table.getItems();
 		for (let index = 0; index < items.length; index++) {
 			const element = items[index];
@@ -153,7 +169,7 @@ describe("Open Excel Upload dialog", () => {
 	it("check Field: Quantity", async () => {
 		const field = await browser.asControl({
 			selector: {
-				id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::OrderItems--com.sap.vocabularies.UI.v1.Identification::quantity::Field"
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::quantity::Field"
 			}
 		});
 		const value = await field.getText();
@@ -163,7 +179,8 @@ describe("Open Excel Upload dialog", () => {
 	it("check Field: Product", async () => {
 		const field = await browser.asControl({
 			selector: {
-				id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::OrderItems--com.sap.vocabularies.UI.v1.Identification::title::Field"
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::title::Field"
+
 			}
 		});
 		const value = await field.getText();
@@ -173,7 +190,7 @@ describe("Open Excel Upload dialog", () => {
 	it("check Field: UnitPrice", async () => {
 		const field = await browser.asControl({
 			selector: {
-				id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::OrderItems--com.sap.vocabularies.UI.v1.Identification::price::Field"
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::price::Field"
 			}
 		});
 		const value = await field.getText();
@@ -181,67 +198,46 @@ describe("Open Excel Upload dialog", () => {
 	});
 
 	it("check Field: validFrom", async () => {
-		const selector = {
+		const field = await browser.asControl({
 			selector: {
-				controlType: "sap.ui.comp.smartform.GroupElement",
-				descendant: {
-					controlType: "sap.ui.comp.smartfield.SmartLabel",
-					properties: {
-						text: "validFrom"
-					}
-				}
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::validFrom::Field"
 			}
-		};
-		const formElement = await browser.asControl(selector);
-		const fields = await formElement.getFields();
-		const field = fields[0];
-		const binding = await field.getBinding("text");
-		const date = await binding.getValue();
+		});
+		const binding = await field.getBindingContext();
+		const object = await binding.getObject();
+		const date = new Date(object.validFrom)
 		const formattedDate = await date.toLocaleString("en-US", optionsLong);
+		// check printend value
 		const valueText = await field.getText();
 		expect(valueText).toBe(formattedDate);
 	});
 
 	it("check Field: timestamp", async () => {
-		const selector = {
+		const field = await browser.asControl({
 			selector: {
-				controlType: "sap.ui.comp.smartform.GroupElement",
-				descendant: {
-					controlType: "sap.ui.comp.smartfield.SmartLabel",
-					properties: {
-						text: "timestamp"
-					}
-				}
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::timestamp::Field"
 			}
-		};
-		const formElement = await browser.asControl(selector);
-		const fields = await formElement.getFields();
-		const field = fields[0];
-		const binding = await field.getBinding("text");
-		const date = await binding.getValue();
+		});
+		const binding = await field.getBindingContext();
+		const object = await binding.getObject();
+		const date = new Date(object.timestamp)
 		const formattedDate = await date.toLocaleString("en-US", optionsLong);
+		// check printend value
 		const valueText = await field.getText();
 		expect(valueText).toBe(formattedDate);
 	});
 
 	it("check Field: date", async () => {
-		const selector = {
+		const field = await browser.asControl({
 			selector: {
-				controlType: "sap.ui.comp.smartform.GroupElement",
-				descendant: {
-					controlType: "sap.ui.comp.smartfield.SmartLabel",
-					properties: {
-						text: "date"
-					}
-				}
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::date::Field"
 			}
-		};
-		const formElement = await browser.asControl(selector);
-		const fields = await formElement.getFields();
-		const field = fields[0];
-		const binding = await field.getBinding("text");
-		const date = await binding.getValue();
+		});
+		const binding = await field.getBindingContext();
+		const object = await binding.getObject();
+		const date = new Date(object.date)
 		const formattedDate = await date.toLocaleString("en-US", optionsShort);
+		// check printend value
 		const valueText = await field.getText();
 		expect(valueText).toBe(formattedDate);
 	});
@@ -249,7 +245,7 @@ describe("Open Excel Upload dialog", () => {
 	it("check Field: time", async () => {
 		const field = await browser.asControl({
 			selector: {
-				id: "ui.v2.ordersv2fe::sap.suite.ui.generic.template.ObjectPage.view.Details::OrderItems--com.sap.vocabularies.UI.v1.Identification::time::Field"
+				id: "ui.v4.ordersv4fe::Orders_ItemsObjectPage--fe::FormContainer::Identification::FormElement::DataField::time::Field"
 			}
 		});
 		const value = await field.getText();
