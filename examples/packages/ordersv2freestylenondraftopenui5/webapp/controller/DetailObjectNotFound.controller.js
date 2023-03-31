e15d24caa7ae3cfde7e5fde8d60ee943e6dff582
marianfoo <13335743+marianfoo@users.noreply.github.com>
2023-03-31 15:57:22 +0200

    test: add openui5 app

---
+++ examples/packages/ordersv2freestylenondraftopenui5/webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["./BaseController"], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("ui.v2.ordersv2freestylenondraftopenui5.controller.DetailObjectNotFound", {});
+});
