3abc24a3bdcc6fb201197581a702cbcac42d9a22
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2023-04-14 15:22:56 +0200

    feat: handle backend error (#110)
    * feat: handle backend error
    
    * feat: handle backend errors
    
    * chore: fix tsconfig
    
    * docs: update

--- packages/ui5-cc-excelUpload/src/controller/Util.ts
+++ packages/ui5-cc-excelUpload/src/controller/Util.ts
@@ -42,14 +42,31 @@ export default class Util {
 	}
 
 	static showError(error: any, className: string, methodName: string) {
-		Log.error(error.message, error, `${className}.${methodName}`);
-		// convert urls to links and to remove lines of the url
-		const regex = /(http[s]?:\/\/[^\s]+):(\d+):(\d+)/g;
-		let errorStack = error.stack.replace(regex, '<a href="$1" target="_blank" class="sapMLnk">$1</a>:<span class="line-no">$2:$3</span>').replace(/\n/g, "<br/>");
-		MessageBox.error(error.message, {
-			details: errorStack,
+		let detailsContent: any = "";
+		let errorMessage = "";
+		try {
+			// code error
+			if (error.stack) {
+				errorMessage = error.message;
+				// convert urls to links and to remove lines of the url
+				const regex = /(http[s]?:\/\/[^\s]+):(\d+):(\d+)/g;
+				let errorStack = error.stack.replace(regex, '<a href="$1" target="_blank" class="sapMLnk">$1</a>:<span class="line-no">$2:$3</span>').replace(/\n/g, "<br/>");
+				detailsContent = errorStack;
+			} else {
+				// OData error
+				const errorObject = JSON.parse(error.responseText);
+				errorMessage = errorObject.error.message.value;
+				detailsContent = errorObject;
+			}
+		} catch (error) {
+			errorMessage = "Generic Error";
+			detailsContent = error;
+		}
+		Log.error(errorMessage, error, `${className}.${methodName}`);
+		MessageBox.error(errorMessage, {
+			details: detailsContent,
 			initialFocus: Action.CLOSE,
-			actions: [Action.CANCEL],
+			actions: [Action.OK],
 		});
 	}
 
