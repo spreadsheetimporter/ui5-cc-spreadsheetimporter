9425bef13d3357c325e8d7d366e703cf6830ae87
marianfoo <13335743+marianfoo@users.noreply.github.com>
2023-04-18 11:50:30 +0200

    chore: fix example apps events

--- examples/packages/ordersv2freestylenondraftopenui5/webapp/controller/List.controller.js
+++ examples/packages/ordersv2freestylenondraftopenui5/webapp/controller/List.controller.js
@@ -81,8 +81,8 @@ sap.ui.define(
 						let errorArray = [];
 						for (const [index, row] of sheetData.entries()) {
 							//check for invalid price
-							if (row.UnitPrice) {
-								if (row.UnitPrice > 100) {
+							if (row["UnitPrice[price]"]) {
+								if (row["UnitPrice[price]"] > 100) {
 									const error = {
 										title: "Price to high (max 100)",
 										row: index + 2,
