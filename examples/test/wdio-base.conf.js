492bf5785183a65a7859f7a80e4bbeeb042476ef
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-05-03 21:43:21 +0200

    test: init BAS

--- examples/test/wdio-base.conf.js
+++ examples/test/wdio-base.conf.js
@@ -15,40 +15,55 @@ const specs = testappObject["testMapping"]["specs"];
 const port = testappObject.port;
 
 module.exports.config = {
+	runner: 'local',
 	wdi5: {
-        logLevel: "error",
-        waitForUI5Timeout: 29000
+        screenshotPath: path.join('wdio-ui5-service', 'test', 'report', 'screenshots'),
+        logLevel: 'verbose',
+        platform: 'browser',
+        url: 'index.html',
+        deviceType: 'web',
     },
 	scenario: scenario,
 	specs: specs,
 	exclude: [
 		// 'path/to/excluded/files'
 	],
-	maxInstances: 10,
+	maxInstances: 1,
 	//
-	capabilities: [
-		{
-			maxInstances: 5,
-			//
-			browserName: "chrome",
-			"goog:chromeOptions": {
-				args:
-					process.argv.indexOf("--headless") > -1
-						? ["--headless=new", "--window-size=1920,1080"]
-						: process.argv.indexOf("--debug") > -1
-						? ["--window-size=1920,1080", "--auto-open-devtools-for-tabs"]
-						: ["--window-size=1920,1080"]
-			},
-			acceptInsecureCerts: true
-		}
-	],
+	capabilities: [{
+		"acceptInsecureCerts": true,
+		"browserName": "firefox",
+		"browserVersion": "102",
+		"moz:firefoxOptions": {
+		  "binary": '/extbin/bin/firefox',
+		  "args": ['-headless', '-profile', '/tmp/profile'],
+		  "log": {"level": "trace"},
+		},
+	}],
 	logLevel: "error",
 	bail: 0,
-	baseUrl: `http://localhost:${port}/index.html?sap-language=EN`,
+	baseUrl: 'http://localhost:8080/index.html',
+	port: 37311,
 	waitforTimeout: 20000,
 	connectionRetryTimeout: process.argv.indexOf("--debug") > -1 ? 1200000 : 120000,
 	connectionRetryCount: 3,
-	services: ["chromedriver", "ui5"],
+	services: [ 
+		[
+		'geckodriver', 
+		// service options 
+		{
+		// OPTIONAL: Arguments passed to geckdriver executable.
+		// Check geckodriver --help for all options. Example:
+		// ['--log=debug', '--binary=/var/ff50/firefox'] 
+		// Default: empty array
+		args: ['--log=trace', '--remote-debugging-port=9222'],
+		
+		// The path where the output of the Geckodriver server should
+		// be stored (uses the config.outputDir by default when not set).
+		 outputDir: './logs' 
+		 }
+		],'ui5'
+	],
 	framework: "mocha",
 	reporters: ["spec"],
 	mochaOpts: {
