7bda6d5a3801844bad96d4ea47454ddfaabf874c
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2023-05-20 17:55:51 +0200

    fix: add missing Edm Types (Decimal, Integer) (#221)
    * fix: add missing Edm Type Decimal
    
    * fix: add missing integer types

--- examples/packages/ordersv2fenondraft/webapp/ext/controller/ObjectPageExt.controller.js
+++ examples/packages/ordersv2fenondraft/webapp/ext/controller/ObjectPageExt.controller.js
@@ -10,7 +10,7 @@ sap.ui.define([], function () {
 					async: true,
 					componentData: {
 						context: this,
-						columns: ["product_ID", "quantity", "title", "price", "validFrom", "timestamp", "date", "time", "boolean"],
+						columns: ["product_ID", "quantity", "title", "price", "validFrom", "timestamp", "date", "time", "boolean", "decimal"],
 						mandatoryFields: ["product_ID", "quantity"],
 						excelFileName: "Test.xlsx"
 					}
