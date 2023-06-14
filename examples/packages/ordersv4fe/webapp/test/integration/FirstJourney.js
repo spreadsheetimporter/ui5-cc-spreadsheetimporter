8b0a080fd802a631b5e1d5b6a1c1b8d05d4d20c9
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-06-14 16:36:48 +0200

    test: new opa5 tests

--- examples/packages/ordersv4fe/webapp/test/integration/FirstJourney.js
+++ examples/packages/ordersv4fe/webapp/test/integration/FirstJourney.js
@@ -1,23 +1,55 @@
-sap.ui.define(["sap/ui/test/opaQunit"], function (opaTest) {
+sap.ui.define(["sap/ui/test/Opa5", "sap/ui/test/opaQunit", "sap/ui/test/actions/Press"], function (Opa5, opaTest, Press) {
 	"use strict";
 
 	var Journey = {
 		run: function () {
 			QUnit.module("First journey");
-
-			opaTest("Start application", function (Given, When, Then) {
-				Given.iStartMyApp();
-
-				Then.onTheOrdersList.iSeeThisPage();
+			// in QUnit start page, before all OPA tests
+			Opa5.extendConfig({
+				autoWait: true
 			});
 
-			opaTest("Navigate to ObjectPage", function (Given, When, Then) {
-				// Note: this test will fail if the ListReport page doesn't show any data
-				When.onTheOrdersList.onFilterBar().iExecuteSearch();
-				Then.onTheOrdersList.onTable().iCheckRows();
+			opaTest("Open Excel Upload Dialog", function (Given, When, Then) {
+				Given.iStartMyApp();
+				Then.waitFor({
+					id: "ui.v4.ordersv4fe::OrdersList--fe::FilterBar::Orders-btnSearch",
+					controlType: "sap.m.Button",
+					visible: true,
+					actions: new Press(),
+					success: function (oControl) {
+						console.log("Yay!");
+					},
+					errorMessage: "Can not select 'sap.m.Button'"
+				});
+				Then.waitFor({
+					id: "ui.v4.ordersv4fe::OrdersList--fe::CustomAction::excelUploadListReport",
+					controlType: "sap.m.Button",
+					visible: true,
+					actions: new Press(),
+					success: function (oControl) {
+						console.log("Yay!");
+					},
+					errorMessage: "Can not select 'sap.m.Button'"
+				});
+			});
 
-				When.onTheOrdersList.onTable().iPressRow(0);
-				Then.onTheOrdersObjectPage.iSeeThisPage();
+			opaTest("Test Util", function (Given, When, Then) {
+				Then.waitFor({
+					controlType: "cc.excelUpload.v0_19_0.ExcelDialog",
+					success: function () {
+						const util = window[0].cc.excelUpload.v0_19_0.Util;
+						var input = "5,6";
+						var expectedOutput = 5;
+						// Call 'changeDecimalSeperator' method and check its output
+						var output = util.changeDecimalSeperator(input);
+						if (output !== expectedOutput) {
+							Opa5.assert.ok(false, "Incorrect decimal seperator");
+						} else {
+							Opa5.assert.ok(true, "Correct decimal seperator");
+						}
+					},
+					errorMessage: "Incorrect decimal seperator"
+				});
 			});
 
 			opaTest("Teardown", function (Given, When, Then) {
