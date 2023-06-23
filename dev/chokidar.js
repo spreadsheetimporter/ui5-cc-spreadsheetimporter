d8f3002cca91b240302dc5293e71b81e492fe957
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-06-23 11:40:16 +0200

    chore: ignore dt.ts files for generator

--- dev/chokidar.js
+++ dev/chokidar.js
@@ -3,7 +3,7 @@ const chokidar = require("chokidar");
 var shell = require("shelljs");
 var execAsync = require("./execAsync")
 let timeoutId;
-// execAsync("npx @ui5/ts-interface-generator --watch")
+execAsync("npx @ui5/ts-interface-generator --watch")
 shell.exec(
 	"pnpm --filter ui5-cc-excelupload build:dev "
 );
@@ -14,7 +14,8 @@ chokidar
 			stabilityThreshold: 1000,
 			pollInterval: 100,
 		},
-		ignoreInitial: true
+		ignoreInitial: true,
+		ignored: '**/*.dt.ts'
 	})
 	.on("change", (event, path) => {
 		clearTimeout(timeoutId)
