8e581ac413459bb431cd021fc6a3e99f68e83eca
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2023-07-21 15:27:53 +0200

    chore: update typescript sample (#309)
    * chore: update typescript sample
    
    * chore: update packages and sample
    
    * fix: working ts sample

--- examples/packages/ordersv4fets/webapp/ext/ObjectPageExtController.ts
+++ examples/packages/ordersv4fets/webapp/ext/ObjectPageExtController.ts
@@ -1,6 +1,6 @@
-import Component, { Component$CheckBeforeReadEventParameters } from "cc/spreadsheetimporter/v0_22_0/Component";
+import Component, { Component$ChangeBeforeCreateEvent, Component$CheckBeforeReadEvent } from "cc/spreadsheetimporter/v0_22_0/Component";
+import BaseController from "sap/fe/core/BaseController";
 import ExtensionAPI from "sap/fe/core/ExtensionAPI";
-import Event from "sap/ui/base/Event";
 /**
  * Generated event handler.
  *
@@ -9,60 +9,50 @@ import Event from "sap/ui/base/Event";
  */
 export async function openSpreadsheetUploadDialog(this: ExtensionAPI) {
 	const view = this.getRouting().getView();
-	const controller = view.getController();
-	let spreadsheetUpload = controller.spreadsheetUpload as Component;
+	const controller = view.getController() as BaseController;
 	view.setBusyIndicatorDelay(0);
 	view.setBusy(true);
-	if (!controller.spreadsheetUpload) {
-		spreadsheetUpload = await this.getRouting()
-			.getView()
-			.getController()
-			.getAppComponent()
-			.createComponent({
-				usage: "spreadsheetImporter",
-				async: true,
-				componentData: {
-					context: this,
-					tableId: "ui.v4.ordersv4fe::OrdersObjectPage--fe::table::Items::LineItem-innerTable",
-					columns: ["product_ID", "quantity", "title", "price", "validFrom", "timestamp", "date", "time", "boolean", "decimal"],
-					mandatoryFields: ["product_ID", "quantity"],
-					spreadsheetFileName: "Test.xlsx"
-				}
-			});
-		controller.spreadsheetUpload = spreadsheetUpload;
-		// event to check before uploaded to app
-		spreadsheetUpload.attachCheckBeforeRead(function (event: Event<Component$CheckBeforeReadEventParameters>) {
-			// example
-			const sheetData = event.getParameter("sheetData");
-			let errorArray = [];
-			for (const [index, row] of sheetData.entries()) {
-				//check for invalid price
-				for (const key in row) {
-					if (key.endsWith("[price]") && row[key].rawValue > 100) {
-						const error = {
-							title: "Price too high (max 100)",
-							row: index + 2,
-							group: true,
-							rawValue: row[key].rawValue,
-							ui5type: "Error"
-						};
-						errorArray.push(error);
-					}
+	const spreadsheetUpload = (await controller.getAppComponent().createComponent({
+		usage: "spreadsheetImporter",
+		async: true,
+		componentData: {
+			context: this,
+			tableId: "ui.v4.ordersv4fe::OrdersObjectPage--fe::table::Items::LineItem-innerTable",
+			columns: ["product_ID", "quantity", "title", "price", "validFrom", "timestamp", "date", "time", "boolean", "decimal"],
+			mandatoryFields: ["product_ID", "quantity"],
+			spreadsheetFileName: "Test.xlsx"
+		}
+	})) as Component;
+	// event to check before uploaded to app
+	spreadsheetUpload.attachCheckBeforeRead((event) => {
+		const sheetData = event.getParameter("sheetData") as any;
+		let errorArray = [];
+		for (const [index, row] of sheetData.entries()) {
+			//check for invalid price
+			for (const key in row) {
+				if (key.endsWith("[price]") && row[key].rawValue > 100) {
+					const error = {
+						title: "Price too high (max 100)",
+						row: index + 2,
+						group: true,
+						rawValue: row[key].rawValue,
+						ui5type: "Error"
+					};
+					errorArray.push(error);
 				}
 			}
-			(event.getSource() as Component).addArrayToMessages(errorArray);
-		}, this);
+		}
+		(event.getSource() as Component).addArrayToMessages(errorArray);
+	});
+	// event to change data before send to backend
+	spreadsheetUpload.attachChangeBeforeCreate((event) => {
+		const payload = event.getParameter("payload") as any;
+		if (payload && payload.price !== undefined) {
+			payload.price = Number(payload.price.toFixed(1));
+			(event.getSource() as Component).setPayload(payload);
+		}
+	});
 
-		// event to change data before send to backend
-		spreadsheetUpload.attachChangeBeforeCreate(function (event: Event<Component$ChangeBeforeCreateEventParameters>) {
-			let payload = event.getParameter("payload");
-			// round number from 12,56 to 12,6
-			if (payload.price) {
-				payload.price = Number(payload.price.toFixed(1));
-			}
-			event.getSource().setPayload(payload);
-		}, this);
-	}
 	spreadsheetUpload.openSpreadsheetUploadDialog();
 	view.setBusy(false);
 }
