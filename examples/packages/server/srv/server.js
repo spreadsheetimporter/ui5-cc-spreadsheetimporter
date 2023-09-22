dae7d57be0b16c4e100241cef7889add9f900af1
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2023-09-22 10:49:32 +0200

    chore: update deps and replace odata v2 adapter (#367)
    * chore: update deps and replace odata v2 adapter
    
    * fix: url
    
    * fix: url

--- examples/packages/server/srv/server.js
+++ examples/packages/server/srv/server.js
@@ -1,4 +1,4 @@
 const cds = require("@sap/cds");
-const cov2ap = require("@sap/cds-odata-v2-adapter-proxy");
+const cov2ap = require("@cap-js-community/odata-v2-adapter");
 cds.on("bootstrap", (app) => app.use(cov2ap()));
 module.exports = cds.server;
