7cb55d04437ccd2b8f2cd9e528684e15722ccba0
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-11-09 19:16:15 +0100

    delete app from cap

--- examples/packages/server/app/ordersv4fecds/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"], function (Component) {
-	"use strict";
-
-	return Component.extend("ui.v4.ordersv4fe.Component", {
-		metadata: {
-			manifest: "json"
-		}
-	});
-});
