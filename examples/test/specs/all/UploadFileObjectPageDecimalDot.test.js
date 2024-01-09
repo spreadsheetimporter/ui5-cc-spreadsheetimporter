486b51e9c0ff5f0df5d1cf39774e6b991ec51c5d
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2024-01-09 11:03:38 +0100

    test: improve objectpagetest

--- examples/test/specs/all/UploadFileObjectPageDecimalDot.test.js
+++ examples/test/specs/all/UploadFileObjectPageDecimalDot.test.js
@@ -26,6 +26,7 @@ describe("Upload File Object Page", () => {
 	});
 
 	it("go to object page", async () => {
+		await BaseClass.dummyWait(2000);
 		const hash = `#/${FE.entitySet}(${FE.entityObjectPageDot})`;
 		await browser.goTo({ sHash: hash });
 		// force wait to stabelize tests
@@ -33,6 +34,7 @@ describe("Upload File Object Page", () => {
 	});
 
 	it("go to edit mode", async () => {
+		await BaseClass.dummyWait(2000);
 		await BaseClass.pressById(FE.objectPageEditButton);
 	});
 
