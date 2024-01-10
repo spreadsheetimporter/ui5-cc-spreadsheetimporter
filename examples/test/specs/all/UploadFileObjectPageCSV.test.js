9a53ed8a2c3ad04418f72e4fc39b3ceeb77b0af7
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2024-01-10 11:38:08 +0100

    test: check edit mode

--- examples/test/specs/all/UploadFileObjectPageCSV.test.js
+++ examples/test/specs/all/UploadFileObjectPageCSV.test.js
@@ -26,15 +26,39 @@ describe("Upload CSV File Object Page", () => {
 	});
 
 	it("go to object page", async () => {
-		await BaseClass.dummyWait(2000);
+		await BaseClass.dummyWait(1000);
 		const hash = `#/${FE.entitySet}(${FE.entityObjectPageCSV})`;
 		await browser.goTo({ sHash: hash });
 		// force wait to stabelize tests
-		await BaseClass.dummyWait(2000);
+		await BaseClass.dummyWait(1000);
 	});
 
 	it("go to edit mode", async () => {
 		await BaseClass.pressById(FE.objectPageEditButton);
+		// check if edit mode is active by checking if the save button is visible
+		await BaseClass.dummyWait(1000);
+		const object = await browser.asControl({
+			forceSelect: true,
+			selector: {
+				id: FE.objectPageSaveButton
+			}
+		});
+		// if the save button is not visible try to press the edit button again
+		if (!object._domId) {
+			await BaseClass.pressById(FE.objectPageEditButton);
+		}
+		// check if edit mode is active by checking if the save button is visible
+		await BaseClass.dummyWait(1000);
+		const object2 = await browser.asControl({
+			forceSelect: true,
+			selector: {
+				id: FE.objectPageSaveButton
+			}
+		});
+		// if the save button is not visible try to press the edit button again
+		if (!object2._domId) {
+			await BaseClass.pressById(FE.objectPageEditButton);
+		}
 	});
 
 	it("Open Spreadsheet Upload Dialog", async () => {
